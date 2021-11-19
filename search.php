5ea01ff7de38f30502f4a440c23a037d56b1cdb6
molynatep <71908029+molynatep@users.noreply.github.com>
2021-11-19 13:43:29 -0500

    search

---
+++ search.php
@@ -0,0 +1,54 @@
+<?php
+include_once __DIR__ . '/_global/header.php';
+?>
+</div>
+    <div class="search">
+        <img src="imgs/search.svg" alt="search">
+    </div>
+    <div class="logo">
+        <img src="imgs/logo.svg" alt="logo">
+    </div>
+</nav>
+<div class="center">
+    <div class= "hero">
+        <h1>Find your favorite recipes.</h1>
+        <h2>What will you cook today?</h2>
+        <input class="searchbar" type="text" placeholder="Search for...">
+</div>
+</div>
+<div class= "recipemargin">
+    <div class="strike">
+        <div class="recipes">
+        <h1>Results for 'Asian Dishes'</h1>
+        </div>
+    </div>
+<div class="grid">
+    <div>
+    <img src="imgs/chicken.png" alt="chicken">
+    </div>
+    <div>
+    <img src="imgs/chicken.png" alt="chicken">
+    </div>
+    <div>
+    <img src="imgs/chicken.png" alt="chicken">
+    </div>
+    <div>
+    <img src="imgs/chicken.png" alt="chicken">
+    </div>
+    <div>
+        <img src="imgs/chicken.png" alt="chicken">
+        </div>
+        <div>
+        <img src="imgs/chicken.png" alt="chicken">
+        </div>
+        <div>
+        <img src="imgs/chicken.png" alt="chicken">
+        </div>
+        <div>
+        <img src="imgs/chicken.png" alt="chicken">
+        </div>
+</div>
+<h2 class="view">VIEW ALL</h2>
+<?php
+include_once __DIR__ . '/_global/footer.php';
+?>
